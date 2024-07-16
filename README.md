89e734d023f55c7a27652b7e6492f5a6eeb4c12c
Fluffy_Kaiju <contact@dreemcloud.net>
2024-07-16 15:05:08 +0000

    chore: wip test file
    * Found the rigth architecture to use and syntax
    * Add a woking hello world example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # libasm
+
+This project is a introduction to the assembly. Here we will create code for the x86_64 architecture following the Intel syntax.
---
+++ main.s
@@ -0,0 +1,20 @@
+; https://jameshfisher.com/2018/03/10/linux-assembly-hello-world/
+; nasm -f elf64 -o main.o main.s && ld -o main main.o && ./main
+global _start
+
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
